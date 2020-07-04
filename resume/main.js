9c0daf16c2ca05dd4fa555f6a63cd78848c0a213
T.A.C.T.I.C.A.L <louis.dalibard@gmail.com>
2020-07-04 18:02:00 +0200

    Update main.js

--- resume/main.js
+++ resume/main.js
@@ -1,45 +1,45 @@
 // Skill Score List
 var scores = ["100%", "95%", "92%", "78%", "77%", "76%", "74%", "100%", "100%", "60%"];
-var filters = "hue-rotate(0deg)";
+var filters = ["hue-rotate(0deg)","hue-rotate(5deg)","hue-rotate(10deg)","hue-rotate(15deg)","hue-rotate(20deg)","hue-rotate(25deg)","hue-rotate(30deg)","hue-rotate(35deg)","hue-rotate(40deg)","hue-rotate(45deg)"];
 $(document).ready(function() {
     setTimeout(function() {
         document.getElementsByClassName("resume-skill-rating")[0].style.width = scores[0];
-        document.getElementsByClassName("resume-skill-rating")[0].style.filter = filters;
+        document.getElementsByClassName("resume-skill-rating")[0].style.filter = filters[0];
     }, 0);
     setTimeout(function() {
         document.getElementsByClassName("resume-skill-rating")[1].style.width = scores[1];
-        document.getElementsByClassName("resume-skill-rating")[1].style.filter = filters;
+        document.getElementsByClassName("resume-skill-rating")[1].style.filter = filters[1];
     }, 50);
     setTimeout(function() {
         document.getElementsByClassName("resume-skill-rating")[2].style.width = scores[2];
-        document.getElementsByClassName("resume-skill-rating")[2].style.filter = filters;
+        document.getElementsByClassName("resume-skill-rating")[2].style.filter = filters[2];
     }, 100);
     setTimeout(function() {
         document.getElementsByClassName("resume-skill-rating")[3].style.width = scores[3];
-        document.getElementsByClassName("resume-skill-rating")[3].style.filter = filters;
+        document.getElementsByClassName("resume-skill-rating")[3].style.filter = filters[3];
     }, 150);
     setTimeout(function() {
         document.getElementsByClassName("resume-skill-rating")[4].style.width = scores[4];
-        document.getElementsByClassName("resume-skill-rating")[4].style.filter = filters;
+        document.getElementsByClassName("resume-skill-rating")[4].style.filter = filters[4];
     }, 200);
     setTimeout(function() {
         document.getElementsByClassName("resume-skill-rating")[5].style.width = scores[5];
-        document.getElementsByClassName("resume-skill-rating")[5].style.filter = filters;
+        document.getElementsByClassName("resume-skill-rating")[5].style.filter = filters[5];
     }, 250);
     setTimeout(function() {
         document.getElementsByClassName("resume-skill-rating")[6].style.width = scores[6];
-        document.getElementsByClassName("resume-skill-rating")[6].style.filter = filters;
+        document.getElementsByClassName("resume-skill-rating")[6].style.filter = filters[6];
     }, 300);
     setTimeout(function() {
         document.getElementsByClassName("resume-skill-rating")[7].style.width = scores[7];
-        document.getElementsByClassName("resume-skill-rating")[7].style.filter = filters;
+        document.getElementsByClassName("resume-skill-rating")[7].style.filter = filters[7];
     }, 350);
     setTimeout(function() {
         document.getElementsByClassName("resume-skill-rating")[8].style.width = scores[8];
-        document.getElementsByClassName("resume-skill-rating")[8].style.filter = filters;
+        document.getElementsByClassName("resume-skill-rating")[8].style.filter = filters[8];
     }, 400);
     setTimeout(function() {
         document.getElementsByClassName("resume-skill-rating")[9].style.width = scores[9];
-        document.getElementsByClassName("resume-skill-rating")[9].style.filter = filters;
+        document.getElementsByClassName("resume-skill-rating")[9].style.filter = filters[9];
     }, 450);
 });
