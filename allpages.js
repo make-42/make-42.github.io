97e13d122184736e609087586ffb12c57841ca34
T.A.C.T.I.C.A.L <louis.dalibard@gmail.com>
2020-07-22 00:21:23 +0200

    Update allpages.js

--- allpages.js
+++ allpages.js
@@ -1,11 +1,17 @@
+let drag = 1.25;
+let layer1speed=0.5;
+let layer2speed=0.75;
+let layer3speed=1;
+
+/* System Varibles*/
 let position = 0;
-let velocity = 0;
+let velocity = 1;
 let oldposition = 0;
 setInterval(function() {
   position += velocity;
-  velocity = Math.floor(velocity/1.25)+(window.scrollY-oldposition);
-  document.getElementsByClassName("layer1")[0].style.backgroundPositionY=position*0.5+"px";
-  document.getElementsByClassName("layer2")[0].style.backgroundPositionY=position*0.75+"px";
-  document.getElementsByClassName("layer3")[0].style.backgroundPositionY=position*1+"px";
+  velocity = Math.round(velocity/drag)+(window.scrollY-oldposition);
+  document.getElementsByClassName("layer1")[0].style.backgroundPositionY=position*layer1speed+"px";
+  document.getElementsByClassName("layer2")[0].style.backgroundPositionY=position*layer2speed+"px";
+  document.getElementsByClassName("layer3")[0].style.backgroundPositionY=position*layer3speed+"px";
   oldposition = window.scrollY;
 },50);
