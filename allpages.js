849f8f5cf23d9bbbf32ba2872ba336bfc9794881
T.A.C.T.I.C.A.L <louis.dalibard@gmail.com>
2020-07-22 00:01:38 +0200

    Update allpages.js

--- allpages.js
+++ allpages.js
@@ -3,10 +3,9 @@ let velocity = 0;
 let oldposition = 0;
 setInterval(function() {
   position += velocity;
-  velocity = Math.round(velocity/2)+(window.scrollY-oldposition);
+  velocity = Math.floor(velocity/1.25)+(window.scrollY-oldposition);
   document.getElementsByClassName("layer1")[0].style.backgroundPositionY=position*0.5+"px";
   document.getElementsByClassName("layer2")[0].style.backgroundPositionY=position*0.75+"px";
   document.getElementsByClassName("layer3")[0].style.backgroundPositionY=position*1+"px";
   oldposition = window.scrollY;
-  console.log(velocity);
 },50);
