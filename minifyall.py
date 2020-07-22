625f544195ad9e40c75f95ae793bc9a3ed074636
T.A.C.T.I.C.A.L <louis.dalibard@gmail.com>
2020-07-22 18:26:08 +0200

    minify everything

--- minifyall.py
+++ minifyall.py
@@ -1,10 +1,36 @@
 import glob
-from slimit import minify
+from jsmin import jsmin
+from rcssmin import cssmin
+import os
 
-for file in glob.glob("./*.js"):
-	fileopen = open(file,"r+")
-	text = str(fileopen.read())
-	fileopen.close()
-	fileopen = open(file+".min.js","w")
-	fileopen.write(minify(text, mangle=True, mangle_toplevel=True))
-	fileopen.close()
+dirs = ["./","./resume/","./stats/","./weather/"]
+
+def minifydir(dir):
+	for file in glob.glob(dir+"*.min.js"):
+		os.remove(file)
+
+	for file in glob.glob(dir+"*.min.css"):
+		os.remove(file)
+
+	for file in glob.glob(dir+"*.js"):
+		fileopen = open(file,"r+", encoding="utf8")
+		text = fileopen.read()
+		fileopen.close()
+		fileopen = open(file[:-3]+".min.js","w", encoding="utf8")
+		fileopen.write(jsmin(text))
+		fileopen.close()
+
+	for file in glob.glob(dir+"*.css"):
+		fileopen = open(file,"r+", encoding="utf8")
+		text = fileopen.read()
+		fileopen.close()
+		fileopen = open(file[:-4]+".min.css","w", encoding="utf8")
+		fileopen.write(cssmin(text))
+		fileopen.close()
+
+
+for loopdir in dirs:
+	minifydir(loopdir)
+
+
+	
