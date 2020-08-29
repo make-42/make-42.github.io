59feffe1d97f2f78c68b2bb2bfefd4df254a72da
T.A.C.T.I.C.A.L <louis.dalibard@gmail.com>
2020-08-29 16:36:05 +0200

    added quotes

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Set Variables For Typing Effect
 var fpstext = 20;
 var fpscursor = 1;
-var sitetitles = ["Louis Dalibard's website", "cd ~/Apps/OpenNotes", "techadvancedcyborg.github.io", "Work in progress...", "cd ~/Apps/TTACT-s-Anime-Player-Revamped", "Why did the chicken cross the road?", "Beans!", "\“Fix the cause, not the symptom.\” – Steve Maguire", "\“Talk is cheap. Show me the code.\” ― Linus Torvalds", "Justice for George Floyd.", "\“I can\'t breathe...\” ― George Floyd"]
+var sitetitles = ["Louis Dalibard's website", "cd ~/Apps/NekoVault", "techadvancedcyborg.github.io", "Converting caffeine to code...", "cd ~/Apps/TTACT-s-Anime-Player-Revamped", "Why did the chicken cross the road?", "Beans!", "\“Fix the cause, not the symptom.\” – Steve Maguire", "\“Talk is cheap. Show me the code.\” ― Linus Torvalds", "Justice for George Floyd.", "\“I can\'t breathe...\” ― George Floyd", "ping 1.1.1.1"]
 //System Variables
 var slice = 0;
 var typing = true;
