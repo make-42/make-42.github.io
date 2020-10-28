3dad545d6915ada5e0f84fb06d97e0ba0dc08b9c
T.A.C.T.I.C.A.L <louis.dalibard@gmail.com>
2020-10-28 16:29:06 +0100

    added dark mode, and light mode switching

--- picotorrent-demo/main.js
+++ picotorrent-demo/main.js
@@ -1,3 +1,18 @@
+darkmode = true;
+
+function toggleDarkMode(){
+  if(darkmode){
+    darkmode = !darkmode;
+    document.body.style.setProperty('--background-color', "#FFF");
+    document.body.style.setProperty('--text-color-light', "#000");
+  } else{
+    darkmode = !darkmode;
+    document.body.style.setProperty('--background-color', "#222");
+    document.body.style.setProperty('--text-color-light', "white");
+  }
+}
+
+
 $('document').ready(function(){
   setInterval(function(){
     document.getElementsByClassName("progress-bar-progress")[0].style.width=Math.random()*100+"%";
