b3e43d1d891338622c6c2e75569a2161b605b6c2
T.A.C.T.I.C.A.L <louis.dalibard@gmail.com>
2020-10-28 17:10:49 +0100

    k

--- picotorrent-demo/main.js
+++ picotorrent-demo/main.js
@@ -1,24 +1,52 @@
 darkmode = true;
 
-function toggleDarkMode(){
-  if(darkmode){
-    darkmode = !darkmode;
-    document.body.style.setProperty('--background-color', "#FFF");
-    document.body.style.setProperty('--text-color-light', "#000");
-  } else{
-    darkmode = !darkmode;
-    document.body.style.setProperty('--background-color', "#222");
-    document.body.style.setProperty('--text-color-light', "white");
-  }
+function toggleDarkMode() {
+    if (darkmode) {
+        darkmode = !darkmode;
+        document.body.style.setProperty('--background-color', "#FFF");
+        document.body.style.setProperty('--text-color-light', "#000");
+    } else {
+        darkmode = !darkmode;
+        document.body.style.setProperty('--background-color', "#222");
+        document.body.style.setProperty('--text-color-light', "white");
+    }
 }
 
 
-$('document').ready(function(){
-  setInterval(function(){
-    var i=0;
-    while(i<document.getElementsByClassName("progress-bar-progress").length){
-    document.getElementsByClassName("progress-bar-progress")[i].style.width=Math.random()*100+"%";
-    i++;
-  }
-  },500)
-});
+$('document')
+    .ready(function() {
+        setInterval(function() {
+            var i = 0;
+            while (i < document.getElementsByClassName("progress-bar-progress")
+                .length) {
+                document.getElementsByClassName("progress-bar-progress")[i].style.width = Math.random() * 100 + "%";
+                i++;
+            }
+        }, 500)
+        /*
+         var i=0;
+            while(i<document.getElementsByClassName("torrent").length){
+            document.getElementsByClassName("torrent")[i].addEventListener("click", function() {
+        var i=0;
+            while(i<document.getElementsByClassName("torrent").length){
+            document.getElementsByClassName("torrent")[i].className = "torrent";
+            i++;
+          }
+        event.srcElement.parentNode.className = "torrent torrent-selected";
+        });
+        document.getElementsByClassName("torrent")[i].addEventListener("mouseover", function() {
+        var i=0;
+            while(i<document.getElementsByClassName("torrent").length){
+            if (document.getElementsByClassName("torrent")[i].className != "torrent torrent-selected"){
+            document.getElementsByClassName("torrent")[i].className = "torrent";
+        }
+            i++;
+          }
+        if (event.srcElement.parentNode.className != "torrent torrent-selected"){
+        event.srcElement.parentNode.className = "torrent torrent-hover";
+        }
+        });
+            i++;
+          }
+        */
+    });
