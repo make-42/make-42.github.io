496a0027311cc3f8900beb15f71ce58781a6245e
TTACTICAL <louis.dalibard@gmail.com>
2020-12-10 21:04:33 +0100

    upload demos

---
+++ assets/demos/scroll-drawing/README.markdown
@@ -0,0 +1,5 @@
+# Scroll Drawing
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/chriscoyier/pen/YXgWam](https://codepen.io/chriscoyier/pen/YXgWam).
+
+As you scroll down, the % scrolled of the document is calculated and that same % of an SVG `<path>` is drawn.
---
+++ assets/demos/scroll-drawing/src/script.js
@@ -0,0 +1,40 @@
+// Get a reference to the <path>
+var path = document.querySelector('#star-path');
+
+// Get length of path... ~577px in this case
+var pathLength = path.getTotalLength();
+
+// Make very long dashes (the length of the path itself)
+path.style.strokeDasharray = pathLength + ' ' + pathLength;
+
+// Offset the dashes so the it appears hidden entirely
+path.style.strokeDashoffset = pathLength;
+
+// Jake Archibald says so
+// https://jakearchibald.com/2013/animated-line-drawing-svg/
+path.getBoundingClientRect();
+
+// When the page scrolls...
+window.addEventListener("scroll", function(e) {
+ 
+  // What % down is it? 
+  // https://stackoverflow.com/questions/2387136/cross-browser-method-to-determine-vertical-scroll-percentage-in-javascript/2387222#2387222
+  // Had to try three or four differnet methods here. Kind of a cross-browser nightmare.
+  var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+    
+  // Length to offset the dashes
+  var drawLength = pathLength * scrollPercentage;
+  
+  // Draw in reverse
+  path.style.strokeDashoffset = pathLength - drawLength;
+    
+  // When complete, remove the dash array, otherwise shape isn't quite sharp
+ // Accounts for fuzzy math
+  if (scrollPercentage >= 0.99) {
+    path.style.strokeDasharray = "none";
+    
+  } else {
+    path.style.strokeDasharray = pathLength + ' ' + pathLength;
+  }
+  
+});
