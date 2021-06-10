be5fe435ba5d6421f27599cb3e84ea92cd272a2f
Louis Dalibard <louis.dalibard@gmail.com>
2021-06-10 21:50:41 +0200

    Update main.js

--- internet-time/main.js
+++ internet-time/main.js
@@ -11,6 +11,19 @@ function itime(){
 }
 
 
+
+var i;
+for (i = 0; i < 10; i++) {
+  var newDiv = document.createElement("div");
+  newDiv.className = "rolling-number rolling-number-"+String(i);
+  newDiv.innerHTML = "0<br>1<br>2<br>3<br>4<br>5<br>6<br>7<br>8<br>9<br>@<br>.";
+  document.getElementsByClassName("time")[0].appendChild(newDiv);
+}
+
 setInterval(function(){
-  document.getElementsByClassName("time")[0].innerHTML = "@"+(Math.round(itime() * 10000) / 10000).toFixed(4);
-},20)
+  var currtimestring = "@"+(Math.round(itime() * 10000) / 10000).toFixed(4).padStart(9, '0');
+  for (i = 0; i < 10; i++) {
+
+      document.getElementsByClassName("rolling-number")[i].style = "transform: translateY(-"+String(120*parseInt(currtimestring[i].replaceAll("@","10").replaceAll(".","11")))+"px);";
+  }
+},100)
